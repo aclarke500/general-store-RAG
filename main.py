9e3f04178abcd75c5ed100d6e1a1f56e619458d9
Adam Clarke <59942887+aclarke500@users.noreply.github.com>
2024-12-13 23:05:07 -0500

    refacted code

--- main.py
+++ main.py
@@ -2,16 +2,20 @@ import lancedb
 from query_utils import query_db, query_LLM
 from intro import display_ascii_art
 
-db = lancedb.connect("./general_store_db")
+def main():
+    db = lancedb.connect("./general_store_db")
 
+    display_ascii_art()
+    message = 'Hi! I am Beedle, your assistant. What are you looking for?'
 
-display_ascii_art()
-message = 'Hi! I am Beedle, your assistant. What are you looking for?'
-while True:
-    user_input = input(message + '\n')
-    response = query_LLM(user_input)
-    print(response)
+    while True:
+        user_input = input(message + '\n')
+        response = query_LLM(user_input)
+        print(response)
 
-    results = query_db(response, db)
-    print(results.head(10))
-    message = 'Is there anything else I can help you with?'
+        results = query_db(response, db)
+        print(results.head(10))
+        message = 'Is there anything else I can help you with?'
+
+if __name__ == "__main__":
+    main()
