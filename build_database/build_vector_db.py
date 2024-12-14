00a4c7aa75a11bd9524752247cbf9e4dc346c718
Vangekillsvado <aclarke500@gmail.com>
2024-12-13 22:33:44 -0500

    began testing

--- build_database/build_vector_db.py
+++ build_database/build_vector_db.py
@@ -1,27 +1,28 @@
-from sentence_transformers import SentenceTransformer
-import numpy as np
+# from sentence_transformers import SentenceTransformer
+# import numpy as np
 import lancedb
 from product_meta_data import ProductMetaData
+from vector_db_utils import create_vector_schema
 
 # Load the SentenceTransformer model
-embedding_length = 768
-model = SentenceTransformer('all-MPNet-base-v2')
+# embedding_length = 768
+# model = SentenceTransformer('all-MPNet-base-v2')
 
-def embed_item(item):
-    description = item['description']   
-    # Embed the description, convert it to vector of 768 in np array
+# def embed_item(item):
+#     description = item['description']   
+#     # Embed the description, convert it to vector of 768 in np array
 
-    embedding = model.encode(description) 
-    normalized_embedding = embedding / np.linalg.norm(embedding) # normalize the vector for more efficient comparison
-    return normalized_embedding
+#     embedding = model.encode(description) 
+#     normalized_embedding = embedding / np.linalg.norm(embedding) # normalize the vector for more efficient comparison
+#     return normalized_embedding
 
-def create_vector_schema(meta_data):
-    vector_schema = [] # list of dictionaries with meta data and vectorized description
-    for item in meta_data:
-        vector = embed_item(item)
-        item['vector'] = vector
-        vector_schema.append(item)
-    return vector_schema 
+# def create_vector_schema(meta_data):
+#     vector_schema = [] # list of dictionaries with meta data and vectorized description
+#     for item in meta_data:
+#         vector = embed_item(item)
+#         item['vector'] = vector
+#         vector_schema.append(item)
+#     return vector_schema 
 
 # connect to LanceDB
 db = lancedb.connect("./general_store_db") # we expect to run this file from root directory
